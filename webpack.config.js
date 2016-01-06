bc38e343ef86db55a12528c947bf18d713a0dc74
Dean McDonnell <mcdonnelldean@outlook.com>
2016-01-06 22:30:23 +0000

    Material UI is now in (but not being used outside a header

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 'use strict'
 
+var Webpack = require('webpack')
+var Path = require('path')
+
+var Transfer = require('transfer-webpack-plugin')
+var UglyifyJs = Webpack.optimize.UglifyJsPlugin
+
+var buildPath = Path.resolve(__dirname, 'lib/bundle')
+var nodeModulesPath = Path.resolve(__dirname, 'node_modules')
+var entryPath = Path.join(__dirname, '/lib/client/app.js')
+var clientPath = Path.resolve(__dirname, "lib/client")
+
 module.exports = {
-  entry: [
-    './lib/client/app.js'
-  ],
+  entry: [entryPath],
+  resolve: ["", ".js", ".jsx"],
   output: {
-    path: './lib/public/js/',
-    filename: 'app.js'
+    path: buildPath,
+    filename: 'js/app.js'
   },
+  plugins: [
+    new UglyifyJs({compress: {warnings: false}}),
+    new Transfer([{from: 'www'}], clientPath)
+  ],
   module: {
     loaders: [{
-      test: /\.js?$/,
-      exclude: '/node_modules/',
-      loader: 'babel?presets[]=react,presets[]=es2015'
+      test: /\.(js|jsx)$/,
+      loader: 'babel?presets[]=react,presets[]=es2015',
+      exclude: [nodeModulesPath],
     }]
   }
 }
