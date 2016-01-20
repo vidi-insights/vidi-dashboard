d8e651b7d7fad5c4988e26db3bfa9301544df605
Karolina Szczur <hi@thefox.is>
2016-01-20 16:18:02 +1100

    add paneled form stylig

--- client/containers/login.js
+++ client/containers/login.js
@@ -28,7 +28,7 @@ export const Login = React.createClass({
       <main className="page page-login" role="main">
         <div className="container-fluid">
           <div className="row middle-xs center-xs vertical-center">
-            <form className="login-form col-xs-12 col-md-6 col-lg-4 txt-left form-full-width" onSubmit={this.handleSubmit}>
+            <form className="login-form col-xs-12 col-md-6 col-lg-4 txt-left form-full-width form-panel" onSubmit={this.handleSubmit}>
               <BoxHeader icon={'icon icon-signin'} title={message} />
                 <input ref="email" type="email" placeholder="Email" className="input-large" required />
                 <input ref="pass" type="password" placeholder="Password" className="input-large" required />
