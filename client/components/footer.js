4c60964047f3e3ad2508a9985bb3676bb3b70af0
Karolina Szczur <hi@thefox.is>
2016-01-21 12:36:55 +1100

    start on structuring overview page

--- client/components/footer.js
+++ client/components/footer.js
@@ -8,7 +8,7 @@ export default React.createClass({
       <footer className="footer txt-small txt-dimmed mb mt txt-center has-icon" role="contentinfo">
         <div className="container-fluid">
           <a href="https://github.com/nearform/vidi-dashboard/issues" className="icon icon-bug icon-dimmed"></a>
-          <p>{'MIT. Copyright (c) 2016. Vidi: Insights'}</p>
+          <p className="m0">{'MIT. Copyright (c) 2016. Vidi: Insights'}</p>
         </div>
       </footer>
     )
