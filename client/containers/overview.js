839cc9641b23db485f5fca9aacf5a672e86f60a0
Karolina Szczur <hi@thefox.is>
2016-01-22 13:28:41 +1100

    basic select styling

--- client/containers/overview.js
+++ client/containers/overview.js
@@ -40,8 +40,18 @@ export const ByService = React.createClass({
       // I don't do anything special with this as it's just a container
       <div className={styleClass}>
         <Sidebar isExpanded={isExpanded} onToggle={handleToggle} />
-        <div className="page">
-          <h2>By Service</h2>
+        <div className="page container-fluid">
+          <div className="row middle-xs">
+            <h2 className="col-xs-12 col-sm-6">Overview</h2>
+            <div className="col-xs-12 col-sm-6 txt-right">
+              <select>
+                <option>120 seconds</option>
+                <option>5 minutes</option>
+                <option>30 minutes</option>
+                <option>1 hour</option>
+              </select>
+            </div>
+        </div>
         </div>
       </div>
     )
