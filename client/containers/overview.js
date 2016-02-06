73dff6c5eee0bc309158a41a72b4d2c029451e6f
malex <mircea.alexandru@gmail.com>
2016-02-06 17:15:14 +0200

    commented some code that creates errors

--- client/containers/overview.js
+++ client/containers/overview.js
@@ -6,11 +6,11 @@ import Panel from '../components/panel'
 
 export const Overview = React.createClass({
   componentDidMount () {
-    this.props.dispatch(subscribe('msgstats', metrics))
+    //this.props.dispatch(subscribe('msgstats', metrics))
   },
 
   componentWillUnmount () {
-    this.props.dispatch(unsubscribe('msgstats', metrics))
+    //this.props.dispatch(unsubscribe('msgstats', metrics))
   },
 
   render () {
