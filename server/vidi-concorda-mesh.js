35553aeb390149b6ebf0db66d2b6a164098320b1
malex <mircea.alexandru@gmail.com>
2016-02-06 17:07:40 +0200

    added back mesh

--- server/vidi-concorda-mesh.js
+++ server/vidi-concorda-mesh.js
@@ -4,6 +4,9 @@ module.exports = (opts, server, done) => {
   var seneca = server.seneca
 
   seneca
+    .use('mesh', {auto: true})
+
+  seneca
     .use('auth', {restrict: '/api'})
 
   // Do fancy concorda and meshing stuff in here only.
@@ -13,7 +16,7 @@ module.exports = (opts, server, done) => {
     method: 'GET',
     path: '/auth/login_google',
     handler: function (request, reply) {
-      reply.redirect('http://localhost:3050/auth/login_google?callback_url=http://localhost:3000/auth/callback')
+      reply.redirect('http://localhost:3050/auth/login_google?callback_url=http://localhost:3000')
     }
   });
 
