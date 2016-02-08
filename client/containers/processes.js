0c49ac6598990699692f463c31a3fc49849fd825
Karolina Szczur <hi@thefox.is>
2016-02-08 17:01:02 +1100

    make processes cards

--- client/containers/processes.js
+++ client/containers/processes.js
@@ -74,46 +74,46 @@ function make_process_sections (data, event_loop) {
   section.push(
     <div key={(now.pid + 'process')}>
       <div className="row middle-xs">
-        <h2 className="col-xs-12 mb2x">{now.pid + '-' + now.title}</h2>
+        <h1 className="col-xs-12 mb mt0">{now.pid + '-' + now.title}</h1>
       </div>
-      <div className="row middle-xs stats-row">
+      <div className="row middle-xs stats-row no-gutter">
         
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3 stats-container stats-floated cf">
+        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4 stats-container stats-floated cf">
           <h1 className="txt-truncate m0">{Math.floor(now.proc_uptime)}</h1>
-          <p className="stats-label m0">Process uptime</p>
+          <p className="label-dimmed m0">Process uptime</p>
         </div>
         
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3 stats-container stats-floated cf">
+        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4 stats-container stats-floated cf">
           <h1 className="txt-truncate m0">{now.sys_uptime}</h1>
-          <p className="stats-label m0">System uptime</p>
+          <p className="label-dimmed m0">System uptime</p>
         </div>
         
       </div>
       
-      <div className="row middle-xs stats-row">
+      <div className="row middle-xs stats-row no-gutter">
         <div className="col-xs-6 col-sm-3 col-md-3 stats-container cf">
           <h2 className="txt-truncate m0">{now.pid}</h2>
-          <p className="stats-label m0">Pid</p>
+          <p className="label-dimmed m0">Pid</p>
         </div>
         
         <div className="col-xs-6 col-sm-3 col-md-3 stats-container cf">
           <h2 className="txt-truncate m0">{now.title}</h2>
-          <p className="stats-label m0">Title</p>
+          <p className="label-dimmed m0">Title</p>
         </div>
         
         <div className="col-xs-6 col-sm-3 col-md-3 stats-container cf">
           <h2 className="txt-truncate m0">{now.arch}</h2>
-          <p className="stats-label m0">Architecture</p>
+          <p className="label-dimmed m0">Architecture</p>
         </div>
         
         <div className="col-xs-6 col-sm-3 col-md-3 stats-container cf">
           <h2 className="txt-truncate m0">{now.platform}</h2>
-          <p className="stats-label m0">Platform</p>
+          <p className="label-dimmed m0">Platform</p>
         </div>
     
       </div>
       
-      <div className="row middle-xs">
+      <div className="row no-gutter middle-xs">
         <h2 className="col-xs-12">Memory Usage</h2>
         <div className="col-xs-12 mtb">
           <ChartistGraph
@@ -146,26 +146,26 @@ function make_event_loop_section (event_loop) {
       <div className="row middle-xs">
         <h2 className="col-xs-12">Event Loop</h2>
       </div>
-      <div className="row middle-xs stats-row">
+      <div className="row middle-xs stats-row no-gutter no-border">
         
         <div className="col-xs-6 col-sm-4 col-md-4 stats-container cf">
           <h2 className="txt-truncate m0">{(Math.round(event_loop.latest.delay * 100) / 100)}</h2>
-          <p className="stats-label m0">Delay</p>
+          <p className="label-dimmed m0">Delay</p>
         </div>
         
         <div className="col-xs-6 col-sm-4 col-md-4 stats-container cf">
           <h2 className="txt-truncate m0">{event_loop.latest.limit}</h2>
-          <p className="stats-label m0">Limit</p>
+          <p className="label-dimmed m0">Limit</p>
         </div>
         
         <div className="col-xs-6 col-sm-4 col-md-4 stats-container cf">
           <h2 className="txt-truncate m0 label-met">{event_loop.latest.over_limit}</h2>
-          <p className="stats-label m0">Over Limit</p>
+          <p className="label-dimmed m0">Over Limit</p>
         </div>
       </div>
       
       <div className="row middle-xs">
-        <div className="col-xs-12 mtb">
+        <div className="col-xs-12 mtbx">
           <ChartistGraph
             type={'Line'}
             data={{labels: event_loop.series.time, series: [event_loop.series.delay, event_loop.series.limit]}}
