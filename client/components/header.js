8949a3fef0a063a6dced3d72e1800c6b248c0053
Karolina Szczur <hi@thefox.is>
2016-02-09 14:46:24 +1100

    redo main navigation to replace sidebar. closes #112

--- client/components/header.js
+++ client/components/header.js
@@ -6,21 +6,29 @@ import {Link} from 'react-router'
 export default React.createClass({
   render () {
     let profile = (
-      <div className="col-xs-4 col-sm-6 txt-right">
-        <ul className="list-unstyled list-inline">
-          <li>
-            <Link to={'/profile'} className="has-icon has-icon-profile">
-              <span className="icon icon-profile"></span>
-              <span>Profile</span>
+      <div className="col-xs-4 col-sm-6 nav-main-wrapper">
+        <input type="checkbox" name="nav-menu-handle" id="nav-menu-handle" className="nav-menu-handle" />
+        <label htmlFor="nav-menu-handle"></label>
+        
+        <nav role="navigation" className="nav-main">
+          
+          <ul className="list-unstyled list-inline">
+            <li className="nav-item"><Link to={'/'}>Overview</Link></li>
+            <li className="nav-item"><Link to={'/processes'}>Processes</Link></li>
+            <li className="nav-item">
+              <Link to={'/profile'} className="has-icon has-icon-profile">
+                <span className="icon icon-profile"></span>
+                <span>Profile</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={'/logout'} className="has-icon has-icon-signout">
+                <span className="icon icon-signout"></span>
+                <span>Sign out</span>
               </Link>
-          </li>
-          <li>
-            <Link to={'/logout'} className="has-icon has-icon-signout">
-              <span className="icon icon-signout"></span>
-              <span>Sign out</span>
-            </Link>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </nav>
       </div>
     )
 
