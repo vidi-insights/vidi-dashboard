205a15526c95dd48334d8906843892adee6fbaac
Dean McDonnell <mcdonnelldean@outlook.com>
2016-02-12 16:30:54 +0000

    removed google login for now

--- client/containers/login.js
+++ client/containers/login.js
@@ -39,13 +39,6 @@ export const Login = React.createClass({
               <input ref="email" type="email" placeholder="Email" className="input-large" required/>
               <input ref="pass" type="password" placeholder="Password" className="input-large" required/>
               <button type="submit" className="btn btn-large submit">Submit</button>
-
-              <div className="panel-footer">
-                <p>Or log in using one of the following services:</p>
-                <a className="btn btn-secondary btn-github has-icon" href="/"><span className="icon icon-google"></span> Github</a>
-                <a className="btn btn-secondary btn-twitter has-icon" href="/"><span className="icon icon-google"></span> Twitter</a>
-                <a className="btn btn-secondary btn-google has-icon" href="/auth/login_google"><span className="icon icon-google"></span> Google</a>
-              </div>
             </form>
           </div>
         </div>
