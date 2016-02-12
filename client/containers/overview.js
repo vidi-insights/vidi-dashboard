340239eb3926c6d0020f94fd4db303930755b040
Karolina Szczur <hi@thefox.is>
2016-02-12 12:06:52 +1100

    dim dead processes

--- client/containers/overview.js
+++ client/containers/overview.js
@@ -73,6 +73,79 @@ export const Overview = React.createClass({
                 <li><span className="status status-small status-terminal"></span><strong>Terminal:</strong> 1</li>
                 <li><span className="status status-small status-dead"></span><strong>Dead:</strong> 5</li>
               </ul>
+              
+              <div className="process-card">
+                <div className="process-heading has-icon">
+                  <span className="status status-healthy status-small" title="Status: healthy"></span> 65296
+                </div>
+                <div className="process-stats-row cf row no-gutter">
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">Process uptime:</h4></li>
+                      <li>02:46:08</li>
+                    </ul>
+                  </div>
+                  
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">System uptime:</h4></li>
+                      <li>02:46:08</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="process-stats-row cf row no-gutter">
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">Heap usage:</h4></li>
+                      <li>73mb out of 106mb (103mb RSS)</li>
+                    </ul>
+                  </div>
+                  
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">Event loop:</h4></li>
+                      <li>4.23s delay (30s limit)</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              
+              <div className="process-card process-dead">
+                <div className="process-heading has-icon">
+                  <span className="status status-dead status-small" title="Status: dead"></span> 65296
+                </div>
+                <div className="process-stats-row cf row no-gutter">
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">Process uptime:</h4></li>
+                      <li>02:46:08</li>
+                    </ul>
+                  </div>
+                  
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">System uptime:</h4></li>
+                      <li>02:46:08</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="process-stats-row cf row no-gutter">
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">Heap usage:</h4></li>
+                      <li>73mb out of 106mb (103mb RSS)</li>
+                    </ul>
+                  </div>
+                  
+                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 process-stats-container process-stats-floated">
+                    <ul className="list-unstyled list-inline cf">
+                      <li><h4 className="m0">Event loop:</h4></li>
+                      <li>50s delay (30s limit)</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              
             </div>
           </div>
 
