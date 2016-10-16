f93c467cb05a41bfa496f88e1ef7f0518adb47fe
Dean McDonnell <mcdonnelldean@outlook.com>
2016-10-16 21:26:03 +0100

    Remove unrelated bits

--- client/lib/createRootComponent.js
+++ client/lib/createRootComponent.js
@@ -8,8 +8,7 @@ import {combineReducers, createStore, applyMiddleware} from 'redux'
 import {browserHistory, Router, Route, IndexRoute} from 'react-router'
 import {routerReducer, routerMiddleware, syncHistoryWithStore} from 'react-router-redux'
 
-import authReducer from '../reducers/auth'
-import vidiReducer from '../reducers/vidi'
+import sensorReducer from '../reducers/sensor'
 
 import {logout, validateCookie} from '../actions/auth'
 import Shell from '../containers/shell'
@@ -22,7 +21,6 @@ import Profile from '../containers/profile'
 
 const rootReducer = combineReducers({
   routing: routerReducer,
-  auth: authReducer,
   vidi: vidiReducer
 })
 
@@ -32,45 +30,17 @@ const buildStore = applyMiddleware(
   loggerMiddleware()
 )(createStore)
 
-const initalState = {
-  auth: {
-    hasError: false,
-    isLoggedIn: false
-  }
-}
+const initalState = {}
 
 const store = buildStore(rootReducer, initalState)
 const history = syncHistoryWithStore(browserHistory, store)
 
 export default function createRootComponent () {
-  function requireAuth (nextState, replace, done) {
-    validateCookie((allowed) => {
-      if(!allowed) {
-        replace({nextPathname: nextState.location.pathname, pathname: '/login', query: nextState.location.query})
-      }
-      else {
-        store.dispatch({type: 'AUTH_VALIDATED', isLoggedIn: true, hasError: false})
-      }
-
-      done()
-    })
-  }
-
-  function handleLogout () {
-    store.dispatch(logout())
-  }
-
   return (
     <Provider store={store}>
       <Router history={history}>
         <Route path="/" component={Shell}>
-          <IndexRoute component={Overview} onEnter={requireAuth}/>
-          <Route path="sensors" component={Sensors}  onEnter={requireAuth}/>
-          <Route path="messages" component={Messages}  onEnter={requireAuth}/>
-          <Route path="process/:id" component={ProcessById}  onEnter={requireAuth}/>
-          <Route path="profile" component={Profile}  onEnter={requireAuth}/>
-          <Route path="login" component={Login} />
-          <Route path="logout" onEnter={handleLogout} />
+          <IndexRoute component={Overview}/>
         </Route>
       </Router>
     </Provider>
